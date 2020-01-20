98ee8de04c1abc6fe71022f495eb4519fa3897b9
David Robillard <d@drobilla.net>
2020-01-20 01:27:03 +0100

    Consider key volume in linear insertion selection

--- include/spaix/LinearInsertion.hpp
+++ include/spaix/LinearInsertion.hpp
@@ -34,22 +34,24 @@ struct LinearInsertion
   static size_t choose(const Children& children, const Key& key)
   {
     using Volume = decltype(volume(std::declval<Key>()));
-    using Sizes  = std::tuple<Volume, ChildCount>;
+    using Sizes  = std::tuple<Volume, Volume, ChildCount>;
 
     size_t best_index = 0;
     Sizes  best_sizes{std::numeric_limits<Volume>::max(),
+                     std::numeric_limits<Volume>::max(),
                      std::numeric_limits<ChildCount>::max()};
 
     for (size_t i = 0; i < children.size() && children[i]; ++i) {
-      const auto& child    = children[i];
-      const auto  expanded = child->key | key;
+      const auto& child           = children[i];
+      const auto  expanded        = child->key | key;
+      const auto  expanded_volume = volume(expanded);
 
       Volume child_expansion = 0;
       if (expanded != child->key) {
-        child_expansion = volume(expanded) - volume(child->key);
+        child_expansion = expanded_volume - volume(child->key);
       }
 
-      Sizes sizes{child_expansion, child->num_children()};
+      Sizes sizes{child_expansion, expanded_volume, child->num_children()};
       if (sizes < best_sizes) {
         best_sizes = std::move(sizes);
         best_index = i;
