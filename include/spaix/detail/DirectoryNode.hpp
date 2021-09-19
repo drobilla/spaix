67e85634444bff7233b522be5ab51ede1369d5fa
David Robillard <d@drobilla.net>
2021-09-18 20:57:30 -0400

    Fix whitespace
    Either things got out of sync with the clang-format configuration, or the
    formatting changed (neither would be surprising).  This is with clang-format
    12.0.1.

--- include/spaix/detail/DirectoryNode.hpp
+++ include/spaix/detail/DirectoryNode.hpp
@@ -29,12 +29,11 @@
 
 namespace spaix {
 
-template <class Key, class Data>
+template<class Key, class Data>
 struct DataNode;
 
-template <class ChildKey, class ChildNode>
-struct NodeEntry
-{
+template<class ChildKey, class ChildNode>
+struct NodeEntry {
   using Key  = ChildKey;
   using Node = ChildNode;
 
@@ -42,44 +41,42 @@ struct NodeEntry
   std::unique_ptr<Node> node{};
 };
 
-template <class DatNode, DataPlacement placement>
+template<class DatNode, DataPlacement placement>
 struct DatEntryType;
 
-template <class DatNode>
-struct DatEntryType<DatNode, DataPlacement::inlined>
-{
+template<class DatNode>
+struct DatEntryType<DatNode, DataPlacement::inlined> {
   using Type = DatNode;
 
-  template <class Key, class Data>
+  template<class Key, class Data>
   static Type make(Key key, Data data)
   {
     return DatNode{std::move(key), std::move(data)};
   }
 };
 
-template <class DatNode>
-struct DatEntryType<DatNode, DataPlacement::separate>
-{
+template<class DatNode>
+struct DatEntryType<DatNode, DataPlacement::separate> {
   using Type = std::unique_ptr<DatNode>;
 
-  template <class Key, class Data>
+  template<class Key, class Data>
   static Type make(Key key, Data data)
   {
     return std::unique_ptr<DatNode>(
-        new DatNode{std::move(key), std::move(data)});
+      new DatNode{std::move(key), std::move(data)});
   }
 };
 
-template <class DirKey,
-          class DatNode,
-          DataPlacement Placement,
-          ChildCount    DirFanout,
-          ChildCount    DatFanout>
-struct DirectoryNode
-{
+template<class DirKey,
+         class DatNode,
+         DataPlacement Placement,
+         ChildCount    DirFanout,
+         ChildCount    DatFanout>
+struct DirectoryNode {
 public:
   using DirNode =
-      DirectoryNode<DirKey, DatNode, Placement, DirFanout, DatFanout>;
+    DirectoryNode<DirKey, DatNode, Placement, DirFanout, DatFanout>;
+
   using Key     = decltype(std::declval<DatNode>().key);
   using Data    = decltype(std::declval<DatNode>().data);
   using NodeKey = DirKey;
@@ -88,14 +85,13 @@ public:
   using DatNodePtr = std::unique_ptr<DatNode>;
 
   using DirEntry = NodeEntry<DirKey, DirNode>;
-  // using DatEntry = DatNodePtr;
-  // using DatEntry = DatNode;
   using DatEntry = typename DatEntryType<DatNode, Placement>::Type;
 
   using DirChildren = StaticVector<DirEntry, ChildCount, DirFanout>;
   using DatChildren = StaticVector<DatEntry, ChildCount, DatFanout>;
 
-  explicit DirectoryNode(const NodeType t) : child_type{t}
+  explicit DirectoryNode(const NodeType t)
+    : child_type{t}
   {
     if (child_type == NodeType::data) {
       new (&dat_children) DatChildren();
@@ -143,70 +139,69 @@ public:
   }
 
   const NodeType child_type; ///< Type of children nodes
-  union
-  {
+  union {
     DirChildren dir_children; ///< Directory node children
     DatChildren dat_children; ///< Data node children
   };
 };
 
-template <class Key, class Node>
+template<class Key, class Node>
 const auto&
 entry_key(const NodeEntry<Key, Node>& entry)
 {
   return entry.key;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_key(const DataNode<Key, Data>& entry)
 {
   return entry.key;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_data(const DataNode<Key, Data>& entry)
 {
   return entry.data;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_data(const std::unique_ptr<DataNode<Key, Data>>& entry)
 {
   return entry->data;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_key(const std::unique_ptr<DataNode<Key, Data>>& entry)
 {
   return entry->key;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_ref(const DataNode<Key, Data>& entry)
 {
   return entry;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto&
 entry_ref(const std::unique_ptr<DataNode<Key, Data>>& entry)
 {
   return *entry;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto*
 entry_ptr(const DataNode<Key, Data>& entry)
 {
   return &entry;
 }
 
-template <class Key, class Data>
+template<class Key, class Data>
 const auto*
 entry_ptr(const std::unique_ptr<DataNode<Key, Data>>& entry)
 {
--- include/spaix/detail/meta.hpp
+++ include/spaix/detail/meta.hpp
@@ -28,73 +28,68 @@
 
 namespace spaix {
 
-template <size_t n, typename... Ts>
+template<size_t n, typename... Ts>
 using Nth = typename std::tuple_element<n, std::tuple<Ts...>>::type;
 
-template <class... Ts>
+template<class... Ts>
 using CommonType = typename std::common_type<Ts...>::type;
 
-template <class Tuple>
-struct CommonScalarType
-{
-};
+template<class Tuple>
+struct CommonScalarType {};
 
-template <class... Ts>
-struct CommonScalarType<std::tuple<Ts...>>
-{
+template<class... Ts>
+struct CommonScalarType<std::tuple<Ts...>> {
   using type = CommonType<Ts...>;
 };
 
-template <size_t i, size_t n>
-struct Index
-{
+template<size_t i, size_t n>
+struct Index {
   constexpr Index<i + 1, n> operator++() const { return {}; }
 };
 
-template <size_t n>
+template<size_t n>
 using EndIndex = Index<n, n>;
 
-template <size_t i, size_t n>
-struct InclusiveIndex
-{
+template<size_t i, size_t n>
+struct InclusiveIndex {
   constexpr InclusiveIndex<i + 1, n> operator++() const { return {}; }
 };
 
-template <size_t n>
+template<size_t n>
 using LastIndex = InclusiveIndex<n, n>;
 
-template <class... Ts>
+template<class... Ts>
 constexpr Index<0, sizeof...(Ts)>
 ibegin()
 {
   return {};
 }
 
-template <class... Ts>
+template<class... Ts>
 constexpr InclusiveIndex<0, sizeof...(Ts) - 1>
 ibegin_inclusive()
 {
   return {};
 }
 
-template <class... Ts, size_t last>
+template<class... Ts, size_t last>
 constexpr auto
 tuple_product(const std::tuple<Ts...>& tuple, InclusiveIndex<last, last>)
 {
   return std::get<last>(tuple);
 }
 
-template <class... Ts, size_t i, size_t last>
+template<class... Ts, size_t i, size_t last>
 constexpr auto
 tuple_product(const std::tuple<Ts...>& tuple, InclusiveIndex<i, last> index)
 {
   return std::get<i>(tuple) * tuple_product(tuple, ++index);
 }
 
-template <class... Ts>
+template<class... Ts>
 using ProductOf =
-    decltype(tuple_product<Ts...>(std::declval<std::tuple<Ts...>>(),
-                                  ibegin_inclusive<Ts...>()));
+  decltype(tuple_product<Ts...>(std::declval<std::tuple<Ts...>>(),
+                                ibegin_inclusive<Ts...>()));
 
 } // namespace spaix
 
