7ed1b8055e28022262f0cd4ecc4a74c1daf0de48
David Robillard <d@drobilla.net>
2022-03-15 19:42:20 -0400

    Add build test for all headers

---
+++ test/test_build.cpp
@@ -0,0 +1,37 @@
+// Copyright 2022 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include "spaix/DataNode.hpp"             // IWYU pragma: keep
+#include "spaix/DataPlacement.hpp"        // IWYU pragma: keep
+#include "spaix/FanoutConfiguration.hpp"  // IWYU pragma: keep
+#include "spaix/Iterator.hpp"             // IWYU pragma: keep
+#include "spaix/LinearInsertion.hpp"      // IWYU pragma: keep
+#include "spaix/LinearSplit.hpp"          // IWYU pragma: keep
+#include "spaix/PageConfiguration.hpp"    // IWYU pragma: keep
+#include "spaix/Point.hpp"                // IWYU pragma: keep
+#include "spaix/QuadraticSplit.hpp"       // IWYU pragma: keep
+#include "spaix/RTree.hpp"                // IWYU pragma: keep
+#include "spaix/RTree.ipp"                // IWYU pragma: keep
+#include "spaix/Rect.hpp"                 // IWYU pragma: keep
+#include "spaix/StaticVector.hpp"         // IWYU pragma: keep
+#include "spaix/contains.hpp"             // IWYU pragma: keep
+#include "spaix/detail/DirectoryNode.hpp" // IWYU pragma: keep
+#include "spaix/detail/distribute.hpp"    // IWYU pragma: keep
+#include "spaix/detail/meta.hpp"          // IWYU pragma: keep
+#include "spaix/draw_dot.hpp"             // IWYU pragma: keep
+#include "spaix/draw_svg.hpp"             // IWYU pragma: keep
+#include "spaix/everything.hpp"           // IWYU pragma: keep
+#include "spaix/intersection.hpp"         // IWYU pragma: keep
+#include "spaix/intersects.hpp"           // IWYU pragma: keep
+#include "spaix/sizes.hpp"                // IWYU pragma: keep
+#include "spaix/touching.hpp"             // IWYU pragma: keep
+#include "spaix/types.hpp"                // IWYU pragma: keep
+#include "spaix/union.hpp"                // IWYU pragma: keep
+#include "spaix/volume.hpp"               // IWYU pragma: keep
+#include "spaix/within.hpp"               // IWYU pragma: keep
+
+int
+main()
+{
+  return 0;
+}
