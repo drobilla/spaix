13c14bc16b854616dcde66a57423389ce1a855b1
David Robillard <d@drobilla.net>
2022-03-16 21:24:37 -0400

    Fix build test

--- test/test_build.cpp
+++ test/test_build.cpp
@@ -1,13 +1,12 @@
 // Copyright 2022 David Robillard <d@drobilla.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include "spaix/Config.hpp"               // IWYU pragma: keep
 #include "spaix/DataNode.hpp"             // IWYU pragma: keep
 #include "spaix/DataPlacement.hpp"        // IWYU pragma: keep
-#include "spaix/FanoutConfiguration.hpp"  // IWYU pragma: keep
 #include "spaix/Iterator.hpp"             // IWYU pragma: keep
 #include "spaix/LinearInsertion.hpp"      // IWYU pragma: keep
 #include "spaix/LinearSplit.hpp"          // IWYU pragma: keep
-#include "spaix/PageConfiguration.hpp"    // IWYU pragma: keep
 #include "spaix/Point.hpp"                // IWYU pragma: keep
 #include "spaix/QuadraticSplit.hpp"       // IWYU pragma: keep
 #include "spaix/RTree.hpp"                // IWYU pragma: keep
@@ -18,15 +17,15 @@
 #include "spaix/detail/DirectoryNode.hpp" // IWYU pragma: keep
 #include "spaix/detail/distribute.hpp"    // IWYU pragma: keep
 #include "spaix/detail/meta.hpp"          // IWYU pragma: keep
-#include "spaix/everything.hpp"           // IWYU pragma: keep
 #include "spaix/intersection.hpp"         // IWYU pragma: keep
 #include "spaix/intersects.hpp"           // IWYU pragma: keep
+#include "spaix/search/everything.hpp"    // IWYU pragma: keep
+#include "spaix/search/touching.hpp"      // IWYU pragma: keep
+#include "spaix/search/within.hpp"        // IWYU pragma: keep
 #include "spaix/sizes.hpp"                // IWYU pragma: keep
-#include "spaix/touching.hpp"             // IWYU pragma: keep
 #include "spaix/types.hpp"                // IWYU pragma: keep
 #include "spaix/union.hpp"                // IWYU pragma: keep
 #include "spaix/volume.hpp"               // IWYU pragma: keep
-#include "spaix/within.hpp"               // IWYU pragma: keep
 
 int
 main()
