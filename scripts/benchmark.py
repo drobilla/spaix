a97974b467b74dabf95f12ee1782443798d19aa5
David Robillard <d@drobilla.net>
2024-10-14 09:48:22 -0400

    Format benchmark script with black

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -34,15 +34,17 @@ class DataSource:
         self.data = pandas.read_csv(path, sep="\t")
 
 
-def plot(sources,
-         out_filename,
-         x_col,
-         x_label,
-         y_col,
-         y_label,
-         show_error=True,
-         y_max=None,
-         y_divisor_col=None):
+def plot(
+    sources,
+    out_filename,
+    x_col,
+    x_label,
+    y_col,
+    y_label,
+    show_error=True,
+    y_max=None,
+    y_divisor_col=None,
+):
     """Plot results"""
 
     matplotlib.use("agg")
@@ -84,8 +86,10 @@ def plot(sources,
 
         yerr = None
         if show_error:
-            yerr = [source.data[y_col] - source.data[min_col],
-                    source.data[max_col] - source.data[y_col]]
+            yerr = [
+                source.data[y_col] - source.data[min_col],
+                source.data[max_col] - source.data[y_col],
+            ]
 
         ax.errorbar(
             source.data[x_col],
@@ -123,9 +127,10 @@ def tsv_path(prog, insert, split):
     #                     "{}_insert_{}_split_{}.tsv".format(
     #                         bench_name(prog), insert, split))))
 
-    return os.path.join(options.dir,
-                        "{}_insert_{}_split_{}.tsv".format(
-                            bench_name(prog), insert, split))
+    return os.path.join(
+        options.dir,
+        "{}_insert_{}_split_{}.tsv".format(bench_name(prog), insert, split),
+    )
 
 
 def run(script_opts, bench_opts, insert, split):
@@ -140,35 +145,56 @@ def run(script_opts, bench_opts, insert, split):
 
 
 if __name__ == "__main__":
-    opt = optparse.OptionParser(usage="%prog [OPTION]...",
-                                description="Benchmark R-tree variants\n")
+    opt = optparse.OptionParser(
+        usage="%prog [OPTION]...", description="Benchmark R-tree variants\n"
+    )
 
-    opt.add_option("--dir", type="string", default=".",
-                   help="path to output directory")
-    opt.add_option("--no-error", action="store_true",
-                   help="do not show error bars")
-    opt.add_option("--no-run", action="store_true",
-                   help="do not run benchmarks")
-    opt.add_option("--no-plot", action="store_true",
-                   help="do not plot benchmarks")
-    opt.add_option("--page-size", type="int", default=512,
-                   help="page size for directory nodes")
-    opt.add_option("--inline", action="store_true",
-                   help="inline data nodes in parents")
-    opt.add_option("--program", type="string",
-                   default=["test/benchmark/bench_spaix_rtree"],
-                   action="append",
-                   help="path to benchmarking program",)
-    opt.add_option("--queries", type="int", default=32,
-                   help="number of queries per step")
-    opt.add_option("--seed", type="int", default=5489,
-                   help="random number generator seed")
-    opt.add_option("--size", type="int", default=1000000,
-                   help="maximum number of elements")
-    opt.add_option("--span", type="float", default=10000000.0,
-                   help="dimension span")
-    opt.add_option("--steps", type="int", default=10,
-                   help="number of benchmarking steps")
+    opt.add_option(
+        "--dir", type="string", default=".", help="path to output directory"
+    )
+    opt.add_option(
+        "--no-error", action="store_true", help="do not show error bars"
+    )
+    opt.add_option(
+        "--no-run", action="store_true", help="do not run benchmarks"
+    )
+    opt.add_option(
+        "--no-plot", action="store_true", help="do not plot benchmarks"
+    )
+    opt.add_option(
+        "--page-size",
+        type="int",
+        default=512,
+        help="page size for directory nodes",
+    )
+    opt.add_option(
+        "--inline", action="store_true", help="inline data nodes in parents"
+    )
+    opt.add_option(
+        "--program",
+        type="string",
+        default=["test/benchmark/bench_spaix_rtree"],
+        action="append",
+        help="path to benchmarking program",
+    )
+    opt.add_option(
+        "--queries", type="int", default=32, help="number of queries per step"
+    )
+    opt.add_option(
+        "--seed", type="int", default=5489, help="random number generator seed"
+    )
+    opt.add_option(
+        "--size",
+        type="int",
+        default=1000000,
+        help="maximum number of elements",
+    )
+    opt.add_option(
+        "--span", type="float", default=10000000.0, help="dimension span"
+    )
+    opt.add_option(
+        "--steps", type="int", default=10, help="number of benchmarking steps"
+    )
 
     (options, args) = opt.parse_args()
     if len(args):
@@ -176,16 +202,23 @@ if __name__ == "__main__":
         sys.exit(1)
 
     if len(options.program) > 1:
-        options.program = options.program[1:] # Remove default
+        options.program = options.program[1:]  # Remove default
 
     bench_opts = [
-        "--page-size", str(options.page_size),
-        "--placement", "inline" if options.inline else "separate",
-        "--queries", str(options.queries),
-        "--seed", str(options.seed),
-        "--size", str(options.size),
-        "--span", str(options.span),
-        "--steps", str(options.steps),
+        "--page-size",
+        str(options.page_size),
+        "--placement",
+        "inline" if options.inline else "separate",
+        "--queries",
+        str(options.queries),
+        "--seed",
+        str(options.seed),
+        "--size",
+        str(options.size),
+        "--span",
+        str(options.span),
+        "--steps",
+        str(options.steps),
     ]
 
     if not options.no_run:
@@ -194,7 +227,7 @@ if __name__ == "__main__":
                 run(options, bench_opts, insert, split)
 
     def mathify(name):
-        return name # "$m$" if name == "linear" else "$m^2$"
+        return name  # "$m$" if name == "linear" else "$m^2$"
 
     if not options.no_plot:
         sources = []
@@ -218,27 +251,64 @@ if __name__ == "__main__":
 
             return result
 
-        plot(sources, os.path.join(options.dir, "insert.svg"),
-             "n", "Size", "t_ins", "Insert time (s)", not options.no_error,
-             max_value(["t_ins"]))
+        plot(
+            sources,
+            os.path.join(options.dir, "insert.svg"),
+            "n",
+            "Size",
+            "t_ins",
+            "Insert time (s)",
+            not options.no_error,
+            max_value(["t_ins"]),
+        )
 
-        plot(sources, os.path.join(options.dir, "throughput.svg"),
-             "n", "Size", "n", "Insert throughput (/s)", False, y_divisor_col="elapsed")
+        plot(
+            sources,
+            os.path.join(options.dir, "throughput.svg"),
+            "n",
+            "Size",
+            "n",
+            "Insert throughput (/s)",
+            False,
+            y_divisor_col="elapsed",
+        )
 
-        plot(sources, os.path.join(options.dir, "iter.svg"),
-             "n", "Size", "t_iter", "Range query time (s)", not options.no_error)
+        plot(
+            sources,
+            os.path.join(options.dir, "iter.svg"),
+            "n",
+            "Size",
+            "t_iter",
+            "Range query time (s)",
+            not options.no_error,
+        )
 
-        plot(sources, os.path.join(options.dir, "q_dirs.svg"),
-             "n", "Size", "q_dirs", "Directory nodes searched",
-             not options.no_error, max_value(["q_dirs_max"]))
+        plot(
+            sources,
+            os.path.join(options.dir, "q_dirs.svg"),
+            "n",
+            "Size",
+            "q_dirs",
+            "Directory nodes searched",
+            not options.no_error,
+            max_value(["q_dirs_max"]),
+        )
 
-        plot(sources, os.path.join(options.dir, "q_dats.svg"),
-             "n", "Size", "q_dats", "Leaf nodes searched",
-             not options.no_error, max_value(["q_dats_max"]))
+        plot(
+            sources,
+            os.path.join(options.dir, "q_dats.svg"),
+            "n",
+            "Size",
+            "q_dats",
+            "Leaf nodes searched",
+            not options.no_error,
+            max_value(["q_dats_max"]),
+        )
 
         html_path = os.path.join(options.dir, "benchmarks.html")
         with open(html_path, "w") as html:
-            html.write("""<!DOCTYPE html>
+            html.write(
+                """<!DOCTYPE html>
 <html lang="en">
   <head><title>R-tree Benchmarks</title>
   <meta charset="utf-8"/></head>
@@ -250,5 +320,6 @@ if __name__ == "__main__":
     <figure><img src="q_dats.svg" alt="Leaf nodes searched"/></figure>
   </body>
 </html>
-""")
+"""
+            )
             sys.stderr.write("Wrote {}\n".format(html_path))
