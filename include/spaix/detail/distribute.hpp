d6e37e556c91a2a82fc75f28962237115626055a
David Robillard <d@drobilla.net>
2025-02-10 18:37:42 -0500

    Avoid double forward

--- include/spaix/detail/distribute.hpp
+++ include/spaix/detail/distribute.hpp
@@ -21,10 +21,10 @@ distribute_child(DirEntry& parent, const DirKey& new_parent_key, Entry&& child)
 
 template<class DirEntry, class Deposit>
 inline void
-distribute_remaining(DirEntry& parent, Deposit&& deposit)
+distribute_remaining(DirEntry& parent, Deposit deposit)
 {
   for (ChildIndex i = 0; i < deposit.size(); ++i) {
-    auto& entry = std::forward<Deposit>(deposit)[i];
+    auto& entry = deposit[i];
     parent.key |= entry_key(entry);
     parent.node->append_child(std::move(entry));
   }
